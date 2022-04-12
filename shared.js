706bc2533235852659982533fe24c1a0641ff190
dasha1994 <kozy.dasha@gmail.com>
2022-04-12 22:11:47 +0300

    js css properties

---
+++ shared.js
@@ -0,0 +1,30 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for(var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+var selectNoButton = document.querySelector('.modal__action--negative');
+selectNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
