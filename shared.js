f71e2ef83c5e0fb7223cd02dd83d490fc2eef85a
dasha1994 <kozy.dasha@gmail.com>
2022-04-13 13:02:01 +0300

    Js added

--- shared.js
+++ shared.js
@@ -6,8 +6,10 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for(var i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
@@ -16,15 +18,19 @@ backdrop.addEventListener('click', function() {
     closeModal();
 });
 
-var selectNoButton = document.querySelector('.modal__action--negative');
-selectNoButton.addEventListener('click', closeModal);
+var modalNoButton = document.querySelector('.modal__action--negative');
+if(modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    if(modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function() {
     mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    backdrop.classList.add('open');
 });
